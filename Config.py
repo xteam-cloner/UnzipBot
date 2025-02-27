7f8831d8b295e7d8a044677d8abde1e96bc6cb0d
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-02-28 00:10:49 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     API_ID = int(os.environ.get("API_ID", 20187096))  # Change 12345 to your API_ID
-    API_HASH = os.environ.get("API_HASH", eddbf671894e0729a62339079fef44b2)  # Change None to your API_HASH
+    API_HASH = os.environ.get("API_HASH", "eddbf671894e0729a62339079fef44b2")  # Change None to your API_HASH
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8060028176:AAEdQYX8GAmJA_KSLjLC_KWhg8Mxfbxhn1c")  # Change None to your BOT_TOKEN
     OWNER_ID = int(os.environ.get("OWNER_ID", 1434595544))  # Change 0 to your OWNER_ID
     OWNER_NAME = os.environ.get("OWNER_NAME", "ᴛᴇᴀᴍx")  # Change None to your OWNER_NAME
